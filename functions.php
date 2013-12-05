910c90779c2da40308c7a85e6b4400d9367b960e
skotperez <voraginebramante@gmail.com>
2013-12-05 16:21:34 +0100

    basic theme files

---
+++ functions.php
@@ -0,0 +1,130 @@
+<?php
+// theme setup main function
+add_action( 'after_setup_theme', 'montera_theme_setup' );
+function montera_theme_setup() {
+
+	// theme global vars
+	if (!defined('MONTERA34_BLOGNAME'))
+	    define('MONTERA34_BLOGNAME', get_bloginfo('name'));
+
+	if (!defined('MONTERA34_BLOGDESC'))
+	    define('MONTERA34_BLOGDESC', get_bloginfo('description','display'));
+
+	if (!defined('MONTERA34_BLOGURL'))
+	    define('MONTERA34_BLOGURL', get_bloginfo('url'));
+
+	if (!defined('MONTERA34_BLOGTHEME'))
+	    define('MONTERA34_BLOGTHEME', get_bloginfo('template_directory'));
+
+	/* Set up media options: sizes, featured images... */
+	add_action( 'init', 'montera34_media_options' );
+
+	/* Add your nav menus function to the 'init' action hook. */
+	add_action( 'init', 'montera34_register_menus' );
+
+	/* Load JavaScript files on the 'wp_enqueue_scripts' action hook. */
+	add_action( 'wp_enqueue_scripts', 'montera34_load_scripts' );
+
+	// Custom post types
+	add_action( 'init', 'montera34_create_post_type', 0 );
+
+	// Custom Taxonomies
+	add_action( 'init', 'montera34_build_taxonomies', 0 );
+
+	// Extra meta boxes in editor
+	//add_filter( 'cmb_meta_boxes', 'montera34_metaboxes' );
+	// Initialize the metabox class
+	//add_action( 'init', 'montera34_init_metaboxes', 9999 );
+
+	// excerpt support in pages
+	add_post_type_support( 'page', 'excerpt' );
+
+	// montera34 shortcodes
+
+} // end montera34 theme setup function
+
+// set up media options
+function montera34_media_options() {
+	/* Add theme support for post thumbnails (featured images). */
+	add_theme_support( 'post-thumbnails', array( 'post','page','project') );
+	set_post_thumbnail_size( 231, 0 ); // default Post Thumbnail dimensions
+	/* set up image sizes*/
+	update_option('thumbnail_size_w', 231);
+	update_option('thumbnail_size_h', 0);
+	update_option('medium_size_w', 474);
+	update_option('medium_size_h', 0);
+	update_option('large_size_w', 717);
+	update_option('large_size_h', 0);
+} // end set up media options
+
+// register custom menus
+function montera34_register_menus() {
+        if ( function_exists( 'register_nav_menus' ) ) {
+                register_nav_menus(
+                array(
+                        'sidebar-menu' => 'Menú lateral',
+                        'sidebar-icon-menu' => 'Menú lateral con iconos',
+                )
+                );
+        }
+} // end register custom menus
+
+// load js scripts to avoid conflicts
+function montera34_load_scripts() {
+	wp_enqueue_script('jquery');
+//	wp_enqueue_script(
+//		'bootstrap.min',
+//		get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js',
+//		array( 'jquery' ),
+//		'2.3.2',
+//		FALSE
+//	);
+
+} // end load js scripts to avoid conflicts
+
+// register post types
+function montera34_create_post_type() {
+	// Documento custom post type
+	register_post_type( 'project', array(
+		'labels' => array(
+			'name' => __( 'Projects' ),
+			'singular_name' => __( 'Project' ),
+			'add_new_item' => __( 'Add a project' ),
+			'edit' => __( 'Edit' ),
+			'edit_item' => __( 'Edit this project' ),
+			'new_item' => __( 'New project' ),
+			'view' => __( 'View project' ),
+			'view_item' => __( 'View this project' ),
+			'search_items' => __( 'Search project' ),
+			'not_found' => __( 'No project found' ),
+			'not_found_in_trash' => __( 'No projects in trash' ),
+			'parent' => __( 'Parent' )
+		),
+		'has_archive' => true,
+		'public' => true,
+		'publicly_queryable' => true,
+		'exclude_from_search' => false,
+		'menu_position' => 5,
+		//'menu_icon' => get_template_directory_uri() . '/images/icon-post.type-integrantes.png',
+		'hierarchical' => false, // if true this post type will be as pages
+		'query_var' => true,
+		'supports' => array('title', 'editor','excerpt','author','comments','trackbacks' ),
+		'rewrite' => array('slug'=>'project','with_front'=>false),
+		'can_export' => true,
+		'_builtin' => false,
+		'_edit_link' => 'post.php?post=%d',
+	));
+} // end register post types
+
+// register taxonomies
+function montera34_build_taxonomies() {
+	register_taxonomy( 'type', 'project', array( // type taxonomy
+		'hierarchical' => true,
+		'label' => __( 'Type' ),
+		'name' => __( 'Types' ),
+		'query_var' => 'type',
+		'rewrite' => array( 'slug' => 'type', 'with_front' => false ),
+	) );
+} // end register taxonomies
+
+?>
---
+++ header.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+
+<html <?php language_attributes(); ?>>
+
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+
+<title>
+<?php
+	/* From twentyeleven theme
+	 * Print the <title> tag based on what is being viewed.
+	 */
+	global $page, $paged;
+
+	wp_title( '|', true, 'right' );
+
+	// Add the blog name.
+	echo MONTERA34_BLOGNAME;
+
+	// Add the blog description for the home/front page.
+	if ( MONTERA34_BLOGDESC && ( is_home() || is_front_page() ) )
+		echo " | " . MONTERA34_BLOGDESC;
+
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 )
+		echo ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
+
+	?>
+</title>
+
+<?php
+// metatags generation
+if ( is_single() || is_page() ) {
+	$metadesc = $post->post_excerpt;
+	if ( $metadesc == '' ) { $metadesc = $post->post_content; }
+	$metadesc = wp_strip_all_tags($post->post_content);
+	$metadesc = strip_shortcodes( $metadesc );
+	$metadesc_fb = substr( $metadesc, 0, 297 );
+	$metadesc_tw = substr( $metadesc, 0, 200 );
+	$metadesc = substr( $metadesc, 0, 154 );
+	$metatit = $post->post_title;
+	$metatype = "article";
+} else {
+	$metadesc = MONTERA34_BLOGDESC;
+	$metadesc_tw = MONTERA34_BLOGDESC;
+	$metadesc_fb = MONTERA34_BLOGDESC;
+	$metatit = "montera34";
+	$metatype = "blog";
+}
+	$metaperma = get_permalink();
+?>
+
+<!-- generic meta -->
+<meta content="montera34" name="author" />
+<meta name="title" content="<?php echo MONTERA34_BLOGNAME ?>" />
+<meta content="<?php echo MONTERA34_BLOGDESC ?>" name="description" />
+<meta content="software libre, open software, software de codigo abierto, datos libres, open data, wordpress, desarrollo web, HTML5, processing, Linux, Debian" name="keywords" />
+<!-- facebook meta -->
+<meta property="og:title" content="<?php echo $metatit ?>" />
+<meta property="og:type" content="<?php echo $metatype ?>" />
+<meta property="og:description" content="<?php echo $metadesc_fb ?>" />
+<meta property="og:url" content="<?php echo $metaperma ?>" />
+<!-- twitter meta -->
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@montera34">
+<meta name="twitter:title" content="<?php echo $metatit ?>" />
+<meta name="twitter:description" content="<?php echo $metadesc_tw ?>" />
+<meta name="twitter:creator" content="@montera34">
+
+<!-- twitter analytics
+<meta property="twitter:account_id" content="1491442110" />-->
+
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+
+<link rel="alternate" type="application/rss+xml" title="<?php echo MONTERA34_BLOGNAME; ?> RSS Feed suscription" href="<?php bloginfo('rss2_url'); ?>" />
+<link rel="alternate" type="application/atom+xml" title="<?php echo MONTERA34_BLOGNAME; ?> Atom Feed suscription" href="<?php bloginfo('atom_url'); ?>" /> 
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+<?php
+if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
+wp_head(); ?>
+
+</head>
+
+<?php // better to use body tag as the main container ?>
+<body <?php body_class(); ?>>
