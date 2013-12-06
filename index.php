19518db0aa9a76737d19851bfaf6702aeedc76e7
skotperez <voraginebramante@gmail.com>
2013-12-06 16:21:09 +0100

    home and type tax archive main title

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
 
 <?php
+// MAIN CONTENT
+
+// home and type tax archive common conditions
 $args = array(
 	'post_type' => 'montera34_project',
 	//'post_type' => 'page',
@@ -10,7 +13,10 @@ $args = array(
 );
 
 if ( is_home() ) {
-// if is home, add sticky project condition to loop
+// if is home
+	// main title
+	$page_tit = "Searching good title for home page";
+	// loop args: add sticky project condition
 	$args['meta_query'] = array(
 		array(
 			'key' => '_montera34_project_sticky',
@@ -18,10 +24,20 @@ if ( is_home() ) {
 			'value' => 'on'
 		),
 	);
-}
+} else {
+	// main title
+	$page_tit = $wp_query->queried_object->name;
+} // end if home
+?>
+<header>
+<h1><?php echo $page_tit ?></h1>
+</header>
 
-$the_query = new WP_Query( $args );
+<section>
+
+<?php $the_query = new WP_Query( $args );
 if ( $the_query->have_posts() ) {
+	$page_tit = get_query_var('montera34_type');
 
 	// The Loop
 	while ( $the_query->have_posts() ) : $the_query->the_post();
@@ -34,6 +50,8 @@ if ( $the_query->have_posts() ) {
 	wp_reset_postdata();
 
 } else {
-	echo "no projects.";
+	echo "<p>no projects.</p>";
 } ?>
+</section>
+
 <?php get_footer(); ?>
