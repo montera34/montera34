8af613331166a46089ac64f2b9b60fb9c68c06d8
skotperez <voraginebramante@gmail.com>
2013-12-09 15:30:08 +0100

    related projects in single

--- single.php
+++ single.php
@@ -49,7 +49,57 @@
 	//wp_reset_postdata();
 
 //} else {
-//} ?>
+//}
+
+// related projects
+if ( $post->post_parent != '0' ) {
+// if project is child
+	$args = array(
+		'post_parent' => $post->post_parent,
+		'posts_per_page' => -1,
+		'exclude' => $post->ID,
+		'post_type' => 'montera34_project'
+	);
+	$related_projects = get_posts($args);
+	$related_count = count($related_projects);
+	$related_type = "child";
+	//if ( $related_count == '0' ) { $related_count = 1; }
+	//echo count($related_projects);
+	//print_r($related_projects);
+	$related_out = "<section><ul>";
+	$related_out .= "<li><a href='" .get_permalink($post->post_parent). "'>" .get_the_title($post->post_parent). "</a></li>";
+	if ( $related_count != '0' ) {
+		foreach ( $related_projects as $related ) {
+			$related_out .= "<li><a href='" .get_permalink($related->ID). "'>" .$related->post_title. "</a></li>";
+		}
+	}
+	$related_out .= "</ul></section>";
+	echo $related_out;
+
+} else {
+// if project is parent
+	$args = array(
+		'post_parent' => $post->ID,
+		'posts_per_page' => -1,
+		'post_type' => 'montera34_project'
+	);
+	$related_projects = get_posts($args);
+	$related_count = count($related_projects);
+
+	if ( $related_count != '0' ) {
+	// if project has children
+		//echo "Project is parent and has children.";
+		$related_out = "<section><ul>";
+		foreach ( $related_projects as $related ) {
+			$related_out .= "<li><a href='" .get_permalink($related->ID). "'>" .$related->post_title. "</a></li>";
+		}
+		$related_out .= "</ul></section>";
+		echo $related_out;
+	}
+} // end if project is parent
+// end related projects
+	
+?>
 
 
 
