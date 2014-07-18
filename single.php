08d68d84476c1af56af8f1a46887dbb015c4fa16
skotperez <voraginebramante@gmail.com>
2014-07-18 13:04:59 +0200

    description in project card and more style stuff in single

--- single.php
+++ single.php
@@ -4,69 +4,86 @@
 if ( have_posts() ) {
 	while ( have_posts() ) : the_post();
 
+// common vars
+$tit = get_the_title();
+$content = apply_filters( 'the_content',get_the_content() );
+$desc = get_the_excerpt();
 
 // if is a project
 if ( get_post_type() == 'montera34_project' ) {
-	// type tax
-	$types = get_the_terms($post->ID,'montera34_type');
-	if ( $types != '' ) {
-		$types_out = "";
-		foreach ( $types as $term ) {
-			$type_name = $term->name;
-			$type_perma = get_term_link($term);
-			$types_out .= "<a href='" .$type_perma. "' title='More projects in " .$type_name. "'>" .$type_name. "</a> ";
+	// card items array
+	// description
+	if ( $desc != '' ) { $card_items['Description'] = $desc; }
+	// type and tech tax
+	$taxes = array(
+		'Type' => 'montera34_type',
+		'Technologies' => 'montera34_tech'
+	);
+	foreach ( $taxes as $tax_tit => $tax ) {
+		$terms = get_the_terms($post->ID,$tax);
+		if ( $terms != '' ) {
+			$terms_out = "";
+			foreach ( $terms as $term ) {
+				$term_name = $term->name;
+				$term_perma = get_term_link($term);
+				$term_out .= "<a href='" .$term_perma. "' title='More projects in " .$term_name. "'>" .$term_name. "</a> ";
+			}
+			$card_items[$tax_tit] = $term_out;
+			unset($term_out);
 		}
-	}
+	} // end loop taxes
+	// year
+	$project_year = get_post_meta( $post->ID, '_montera34_project_card_date_ini', true );
+	if ( $project_year != '' ) { $card_items['Year'] = $project_year; }
+	// client
+	$project_client = get_post_meta( $post->ID, '_montera34_project_card_client', true );
+	if ( $project_client != '' ) { $card_items['Client'] = $project_client; }
 	// project URL
-	$project_url = get_post_meta( $post->ID, '_montera34_project_card_url', true );
-	if ( $project_url != '' ) { $project_url_out = "<a href='" .$project_url. "'>" .$project_url. "</a>"; }
+	$project_url = get_post_meta( $post->ID, '_montera34_project_card_project_url', true );
+	if ( $project_url != '' ) { $card_items['Project URL'] = "<a href='" .$project_url. "'>" .$project_url. "</a>"; }
 	// project code repo URL
-	$project_code_repo = get_post_meta( $post->ID, '_montera34_project_card_code_repo', true ); //print_r($project_code_repo);
-	if ( $project_code_repo != '' ) { $project_code_repo_out = "<a href='" .$project_code_repo[0]['url']. "'>" .$project_code_repo[0]['url_text']. "</a>"; }
+	$project_code_repo = get_post_meta( $post->ID, '_montera34_project_card_code_repo', true ); 
+	if ( $project_code_repo != '' ) { $card_items['Code repository'] = "<a href='" .$project_code_repo[0]['url']. "'>" .$project_code_repo[0]['url_text']. "</a>"; }
 	// project code license URL
 	$project_code_license = get_post_meta( $post->ID, '_montera34_project_card_code_license', true );
-	if ( $project_code_license != '' ) { $project_code_license_out = "<a href='" .$project_code_license[0]['url']. "'>" .$project_code_license[0]['url_text']. "</a>"; }
+	if ( $project_code_license != '' ) { $card_items['Code license'] = "<a href='" .$project_code_license[0]['url']. "'>" .$project_code_license[0]['url_text']. "</a>"; }
+	// status
+	$project_status = get_post_meta( $post->ID, '_montera34_project_card_status', true );
+	if ( $project_status != '' ) { $card_items['Status'] = $project_status; }
 
-	// card items array
-	$card_items = array(
-		'Type' => $types_out,
-		'Status'=> get_post_meta( $post->ID, '_montera34_project_card_status', true ),
-		'Project URL' => $project_url_out,
-		'Client' => get_post_meta( $post->ID, '_montera34_project_card_client', true ),
-		'Code repository' => $project_code_repo_out,
-		'Code license' => $project_code_license_out,
-	);
 	// building collaborators section
 	$project_collaboras = get_post_meta( $post->ID, '_montera34_collabora', true );
-	$args = array(
-		'posts_per_page' => -1,
-		'post__in' => $project_collaboras,
-		'post_type' => 'montera34_collabora'
-	);
-	$collaboras = get_posts($args);
-	if ( count($collaboras) != 0 ) {
-	$collaboras_out = "<section id='collaborators'><h3>Collaborators</h3><div class='media-list'>";
-		foreach ( $collaboras as $collabora ) {
-			$collabora_perma = get_permalink($collabora->ID);
-			$collabora_tit = $collabora->post_title;
-			if ( has_post_thumbnail($collabora->ID) ) {
-				$collabora_img = get_the_post_thumbnail( $collabora->ID, 'bigicon', array('class' => 'media-object') );
-				$collabora_img_out = "<a class='pull-left' href='" .$collabora_perma. "' title='" .$collabora_tit. "'>" .$collabora_img. "</a>";
-			} else { $collabora_img_out = ""; }
-			$collabora_roles = get_post_meta( $collabora->ID, '_montera34_collabora_projects', true );
-			foreach ( $collabora_roles as $rol ) {
-				if ( $rol['project'] == $post->ID ) { $collabora_rol = $rol['rol']; }
-			}
-			$collaboras_out .=
-			"<div class='media'>" .$collabora_img_out. "
-				<div class='media-body'>
-					<h4 class='media-heading'><a href='" .$collabora_perma. "' title='" .$collabora_tit. "'>" .$collabora_tit. "</a></h4>
-					<p>" .$collabora_rol. "</p>
-			</div>
-			";
-		} // end foreach collaborators
-	} // end if collaborators
-	$collaboras_out .= "</div></section>";
+	if ( $project_collaboras != '' ) {
+		$args = array(
+			'posts_per_page' => -1,
+			'post__in' => $project_collaboras,
+			'post_type' => 'montera34_collabora'
+		);
+		$collaboras = get_posts($args);
+		if ( count($collaboras) != 0 ) {
+		$collaboras_out = "<section id='collaborators'><h3>Collaborators</h3><div class='media-list'>";
+			foreach ( $collaboras as $collabora ) {
+				$collabora_perma = get_permalink($collabora->ID);
+				$collabora_tit = $collabora->post_title;
+				if ( has_post_thumbnail($collabora->ID) ) {
+					$collabora_img = get_the_post_thumbnail( $collabora->ID, 'bigicon', array('class' => 'media-object') );
+					$collabora_img_out = "<a class='pull-left' href='" .$collabora_perma. "' title='" .$collabora_tit. "'>" .$collabora_img. "</a>";
+				} else { $collabora_img_out = ""; }
+				$collabora_roles = get_post_meta( $collabora->ID, '_montera34_collabora_projects', true );
+				foreach ( $collabora_roles as $rol ) {
+					if ( $rol['project'] == $post->ID ) { $collabora_rol = $rol['rol']; }
+				}
+				$collaboras_out .=
+				"<div class='media'>" .$collabora_img_out. "
+					<div class='media-body'>
+						<h4 class='media-heading'><a href='" .$collabora_perma. "' title='" .$collabora_tit. "'>" .$collabora_tit. "</a></h4>
+						<p>" .$collabora_rol. "</p>
+				</div>
+				";
+			} // end foreach collaborators
+		} // end if collaborators
+		$collaboras_out .= "</div></section>";
+	} // end if there is collaboras
 
 	// related projects
 	if ( $post->post_parent != '0' ) {
@@ -125,25 +142,18 @@ if ( get_post_type() == 'montera34_project' ) {
 	// card items array
 	// collaborator URL
 	$collabora_url = get_post_meta( $post->ID, '_montera34_collabora_url', true );
-	if ( $collabora_url != '' ) { $collabora_url_out = "<a href='" .$collabora_url. "'>" .$collabora_url. "</a>"; }
+	if ( $collabora_url != '' ) { $card_items['Website'] = "<a href='" .$collabora_url. "'>" .$collabora_url. "</a>"; }
 	// twitter URL
 	$collabora_twitter = get_post_meta( $post->ID, '_montera34_collabora_twitter', true );
-	if ( $collabora_twitter != '' ) { $collabora_twitter_out = "<a href='" .$collabora_twitter. "'>" .$collabora_twitter. "</a>"; }
+	if ( $collabora_twitter != '' ) { $card_items['Twitter'] = "<a href='" .$collabora_twitter. "'>" .$collabora_twitter. "</a>"; }
 	$card_items = array(
 		'Firstname' => get_post_meta( $post->ID, '_montera34_collabora_firstname', true ),
 		'Lastname' => get_post_meta( $post->ID, '_montera34_collabora_lastname', true ),
-		'Website' => $collabora_url_out,
-		'Twitter' => $collabora_twitter_out,
 	);
 	$collaboras_out = "";
 	$related_out = "";
 
 } // end vars depending on post type
-
-// common vars
-$tit = get_the_title();
-$content = apply_filters( 'the_content',get_the_content() );
-$desc = ": " .strip_tags( get_the_excerpt() );
 ?>
 
 		<header>
