2be9824c20ff8a7f5fdd8f69b9e4f2b48b7b0eee
skotperez <voraginebramante@gmail.com>
2014-07-20 19:07:13 +0200

    project loop new structure and styles

--- loop.php
+++ loop.php
@@ -1,37 +1,69 @@
 <?php
-// title
-if ( is_home() ) {
-	$terms = get_the_terms($post->ID,'montera34_type');
-	foreach ( $terms as $term ) {
-		$loop_tit = $term->name. " [+]";
-		$term_perma = get_term_link($term);
-	}
-	$loop_desc = get_the_title();
-	$loop_excer = get_the_excerpt();
-} else {
-	$loop_tit = get_the_title();
-	$loop_desc = get_the_excerpt();
-}
+// projects list loop
 
+// common vars
 // permalink
 $project_perma = get_permalink();
-
 // featured image
 if ( has_post_thumbnail() ) {
-	$art_featured = get_the_post_thumbnail($post->ID,'thumbnail',array('class' => 'img-responsive')); // falta configurar $size correctamente
-	$loop_featured = "<figure>" .$art_featured. "</figure>";
+	$loop_featured = "<figure><a href=" .$project_perma. ">" .get_the_post_thumbnail($post->ID,'thumbnail',array('class' => 'img-responsive')). "</a></figure>";
 } else { $loop_featured = ""; }
 
+$loop_desc = get_the_excerpt();
+
+// vars depending on the view
+if ( is_home() ) {
+	$loop_tag = "section";
+	$loop_class = "list-home";
+	$terms = get_the_terms($post->ID,'montera34_type');
+	foreach ( $terms as $term ) {
+		$loop_tit = $term->name;
+		$loop_perma = get_term_link($term);
+	}
+	$loop_subtit = "<strong>" .get_the_title(). "</strong>. ";
+	$loop_terms = "";
+
+} elseif ( is_tax() ) {
+	$loop_tag = "article";
+	$loop_class = "list-tax";
+	$loop_tit = get_the_title();
+	$loop_perma = $project_perma;
+	$loop_subtit = "";
+	$terms = get_the_terms($post->ID,'montera34_tech');
+	if ( count($terms) > 0 ) {
+		$loop_terms = "<ul class='list-inline'>";
+		foreach ( $terms as $term ) {
+			$term_tit = $term->name;
+			$term_perma = get_term_link($term);
+			$loop_terms .= "<li><a href='" .$term_perma. "'>" .$term_tit. "</a></li>";
+		}
+		$loop_terms .= "</ul>";
+	} else { $loop_terms = ""; }
+
+} else {
+	$loop_tag = "article";
+	$loop_class = "list-project";
+	$loop_tit = get_the_title();
+	$loop_perma = $project_perma;
+	$loop_subtit = "";
+	$terms = get_the_terms($post->ID,'montera34_type');
+	$loop_terms = "<ul class='list-inline'>";
+	foreach ( $terms as $term ) {
+		$term_tit = $term->name;
+		$term_perma = get_term_link($term);
+		$loop_terms .= "<li><a href='" .$term_perma. "'>" .$term_tit. "</a></li>";
+	}
+	$loop_terms .= "</ul>";
+
+}
 ?>
 
-<article>
-<?php if ( is_home() ) {?>
-	<header><h2><a href="<?php echo $term_perma ?>"><?php echo $loop_tit ?></a></h2></header>
-	<strong><a href="<?php echo $project_perma ?>"><?php echo $loop_desc ?></a></strong><br>
-	<?php echo $loop_excer ?>	
-<?php } else { ?>
-	<header><h2><a href="<?php echo $project_perma ?>"><?php echo $loop_tit ?></a></h2></header>
-	<?php echo $loop_desc ?>
-<?php } ?>
-<a href="<?php echo $project_perma ?>"><?php echo $loop_featured ?></a>
-</article>
+<<?php echo $loop_tag ?> class="list-item <?php echo $loop_class ?>">
+	<header><h2 class="list-item-tit"><a href="<?php echo $loop_perma ?>"><?php echo $loop_tit ?></a></h2></header>
+	<div class="list-item-desc">
+		<?php echo $loop_terms ?>
+		<p><?php echo $loop_subtit . $loop_desc; ?></p>
+		<?php echo $loop_featured ?>
+	</div>
+</<?php echo $loop_tag ?>>
+
