edaec9382919d1ce238ec052331ad92c0c15180e
skotperez <voraginebramante@gmail.com>
2014-09-21 21:11:16 +0200

    404 template

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+		<header class="main-tit">
+			<h1>Error 404</h1>
+		</header>
+		<div class="row">
+			<div class="col-md-9">
+				<p>No content</p>
+			</div>
+
+		</div><!-- .row -->
+
+<?php get_footer(); ?>
