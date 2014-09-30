8203e9621babe5f6522cb6f1b64ed8b3a285aae5
skotperez <voraginebramante@gmail.com>
2014-09-30 08:26:29 +0200

    remove private content from single loops

--- single.php
+++ single.php
@@ -65,6 +65,7 @@ if ( get_post_type() == 'montera34_project' ) {
 		$args = array(
 			'posts_per_page' => -1,
 			'post__in' => $project_collaboras,
+			'post_status' => array( 'publish' ),
 			'post_type' => 'montera34_collabora'
 		);
 		$collaboras = get_posts($args);
@@ -175,7 +176,7 @@ if ( get_post_type() == 'montera34_project' ) {
 		$args = array(
 			'posts_per_page' => -1,
 			'post__in' => $project_ids,
-			'post_status' => array( 'publish', 'private' ),
+			'post_status' => array( 'publish' ),
 			'post_type' => 'montera34_project',
 			'order' => 'DESC',
 			'orderby' => 'meta_value_num title',
