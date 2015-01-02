9c54919dab9c9d3a6610c51365440bd51f3057ee
skotperez <voraginebramante@gmail.com>
2015-01-02 11:49:02 +0100

    change order in projects list in home

--- functions.php
+++ functions.php
@@ -527,6 +527,9 @@ function montera34_custom_args_for_loops( $query ) {
 			),
 		));
 		$query->set( 'post_type',array('montera34_project'));
+		$query->set( 'order','DESC');
+		$query->set( 'orderby','meta_value_num title');
+		$query->set( 'meta_key','_montera34_project_card_date_ini');
 	}
 	if ( !is_admin() && is_post_type_archive('montera34_project') && false == $query->query_vars['suppress_filters'] ||
 	!is_admin() && is_tax('montera34_type') && false == $query->query_vars['suppress_filters'] ) {
